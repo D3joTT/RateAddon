768ced55c9fe2e0a878250f9bd45776d68f7776b
d3jott <djpromilly@gmail.com>
2021-04-10 23:41:56 +0200

    Added rating system

--- src/main/java/net/d3jott/rateaddon/commands/GiveHearth.java
+++ src/main/java/net/d3jott/rateaddon/commands/GiveHearth.java
@@ -1,13 +1,32 @@
 package net.d3jott.rateaddon.commands;
 
+import com.plotsquared.core.api.PlotAPI;
+import com.plotsquared.core.plot.Plot;
+import com.plotsquared.core.plot.Rating;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class GiveHearth implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (cmd.getName().equalsIgnoreCase("serce")) {
+
+            if (sender instanceof Player) {
+
+                Player pl = Bukkit.getPlayer(sender.getName());
+
+                PlotAPI plotAPI = new PlotAPI();
+                Rating rating = new Rating(10);
+
+                Plot.getPlot(plotAPI.wrapPlayer(pl.getUniqueId()).getLocation()).addRating(pl.getUniqueId(), rating);
+            }
+            return true;
+        }
 
         return false;
     }
