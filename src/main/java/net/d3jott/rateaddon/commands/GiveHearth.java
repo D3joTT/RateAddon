b70c12da9e7ac0bcb97d0b1a4431e6e874258bd4
d3jott <djpromilly@gmail.com>
2021-04-10 23:41:15 +0200

    Added command /serce

---
+++ src/main/java/net/d3jott/rateaddon/commands/GiveHearth.java
@@ -0,0 +1,14 @@
+package net.d3jott.rateaddon.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class GiveHearth implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        return false;
+    }
+}
